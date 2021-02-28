5f130a820c0a2cbf3c14329249744331bb91190a
supertory <supertory@outlook.com>
2021-03-01 00:11:11 +0800

    +84、柱状图最大面积

--- src/Test.java
+++ src/Test.java
@@ -15,41 +15,8 @@ public class Test {
         a[j] = t;
     }
 
-    public static int partition(int[] arr, int low, int high) {
-        int flag = arr[low];     // 基准数据
-        while (low < high) {
-            // 从队尾向左移动找到小于flag的元素，并移动到low指针所在位置
-            while (low < high && arr[high] >= flag) high--;
-            arr[low] = arr[high];
-            // 从队首向右查找大于flag的元素，移动到刚才high指针的那个位置
-            while (low < high && arr[low] <= flag) low++;
-            arr[high] = arr[low];
-        }
-        //左边小于flag，右边都大于flag，此时low指针的位置就是flag正确的位置
-        arr[low] = flag;
-        return low; // 返回划分的位置
-    }
-
-    public static void quickSort(int[] arr, int low, int high) {
-        if (low < high) {
-            int mid = partition(arr, low, high);    //找到划分的位置
-            quickSort(arr, low, mid - 1);       //对前面的进行排序
-            quickSort(arr, mid + 1, high);      //对后面的进行排序
-        }
-    }
-
     public static void main(String[] args) {
-//        List<Character> characters=new ArrayList<>();
-//        characters.add('a');
-//        characters.add('b');
-//        String s1=String.valueOf(characters);
-
-        TreeNode tree=new TreeNode(new int[]{1,2,3,0,0,4,5});
-        String str=tree.toString();
-        System.out.println(str);
-        TreeNode nt=tree.deserialize(str);
-        System.out.println(nt.left.val);
-
+        System.out.println(3/2*2);
     }
 
 }
