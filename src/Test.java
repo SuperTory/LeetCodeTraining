8eab1be15f4fda0f85e361d4b074ddeb8121315a
Tory <SuperTory@outlook.com>
2021-09-07 21:30:02 +0800

    2021年9月7日

--- src/Test.java
+++ src/Test.java
@@ -5,36 +5,23 @@ import java.util.Deque;
 import java.util.List;
 
 public class Test {
-    List<List<Integer>> res = new ArrayList<>();
+    private int[] sum;
 
-    public List<List<Integer>> subsetsWithDup(int[] nums) {
-        Arrays.sort(nums);
-        List<Integer> numList = new ArrayList<>();
-        boolean[] visited = new boolean[nums.length];
-        subset(nums, 0, numList, visited);
-        return res;
+    public Test(int[] nums) {
+        sum = new int[nums.length + 1];
+        sum[0] = 0;
+        for (int i = 1; i <= nums.length; i++) {
+            sum[i] = sum[i - 1] + nums[i - 1];
+        }
     }
 
-    void subset(int[] nums, int index, List<Integer> numList, boolean[] visited) {
-        if (index == nums.length) {
-            res.add(new ArrayList<>(numList));
-            return;
-        }
-
-        subset(nums, index + 1, numList, visited);
-        if (index > 0 && nums[index] == nums[index - 1] && !visited[index - 1])
-            return;
-        numList.add(nums[index]);
-        visited[index] = true;
-        subset(nums, index + 1, numList, visited);
-        numList.remove(numList.size() - 1);
-        visited[index] = false;
-
+    public int sumRange(int left, int right) {
+        return sum[right + 1] - sum[left];
     }
 
     public static void main(String[] args) {
-        Test t = new Test();
-        List<List<Integer>> res = t.subsetsWithDup(new int[]{1, 2, 2});
+        Test t = new Test(new int[]{-2, 0, 3, -5, 2, -1});
+        int res = t.sumRange(2, 5);
         System.out.println(res);
     }
 }
